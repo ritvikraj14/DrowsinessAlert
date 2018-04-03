ed9a548aac5f8a8181b4776b968dfb4b01a39579
Ritvik Raj <ritvikraj13@gmail.com>
2018-04-03 14:54:17 +0530

    Update credentials.py

--- twilio1/credentials.py
+++ twilio1/credentials.py
@@ -1,4 +1,4 @@
-account_sid = "ACb9f3d225b2e27b63265fd1f184bd2e3d"
-auth_token = "16a84a67a9f4abb1bef00a5269d6c55c"
-my_cell = "+917060334053"
-my_twilio = "+13232759475"
+account_sid = "generated after making account on twilio"
+auth_token = "generated after making account on twilio"
+my_cell = "+your_mobile_number"
+my_twilio = "+generated after making account on twilio"
